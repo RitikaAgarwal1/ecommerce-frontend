af90c46d4fba01e713f010d22b147ac1692552f8
Ritika Agarwal <Ritika.Agarwal1@gds.ey.com>
2021-06-22 21:53:54 +0530

    added email template

--- src/components/Admin/Admin.js
+++ src/components/Admin/Admin.js
@@ -96,7 +96,7 @@ const Admin = props => {
       });
       setIsLoader(false);
     } else {
-      // await axios.post(`${configData.BASEURL}register`, data).then (async res => {
+      await axios.post(`${configData.BASEURL}register`, data).then (async res => {
         const info = {
           content: `<div style="width: 100%;background:#eee;padding:1%;">
                     <div style="border: 2px solid #c2d44e;color:#646565;width: 650px;margin: auto;font-family: system-ui;">
@@ -131,15 +131,15 @@ const Admin = props => {
 
         fetchUsers('user_role', 'ADMIN', 'company_name', 'ASC', 5, 0);
 
-      // }).catch(err => {
-      //   console.log(err);
-      //   setIsLoader(false);
-      //   addToast("Data is not properly filled up!", {
-      //     appearance: 'error',
-      //     autoDismiss: true,
-      //     placement: "bottom-center"
-      //   });
-      // });
+      }).catch(err => {
+        console.log(err);
+        setIsLoader(false);
+        addToast("Data is not properly filled up!", {
+          appearance: 'error',
+          autoDismiss: true,
+          placement: "bottom-center"
+        });
+      });
     }
   }
 
