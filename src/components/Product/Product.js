834508511facd8003446488366f169e389215765
Ritika Agarwal <Ritika.Agarwal1@gds.ey.com>
2021-07-09 18:28:37 +0530

    need to work on filter method

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -15,12 +15,15 @@ const Product = props => {
   const [order, setOrder] = useState('ASC');
   const [orderBy, setOrderBy] = useState('title');
   const [showLoadBtn, setLoadBtn] = useState(true);
+  const [showFilter, setShowFilter] = useState(false);
   const [offsetValue, setOffset] = useState(0);
   const [brand, setBrand] = useState([]);
   const [category, setCategory] = useState([]);
   const [color, setColor] = useState([]);
   const [offer, setOffer] = useState([]);
   const [filterData, setFilterData] = useState([]);
+  const [columnName, setColumnName] = useState([]);
+  const [selectedData, setSelectedData] = useState([]);
 
   const size = ['XS', 'S', 'M', 'L', 'XL', 'XXL'];
   const price = ['Rs 500 - Rs 1000', 'Rs 1000 to Rs 2000', 'Rs 2000 to Rs 3000', 'Rs 3000 to Rs 4000'];
@@ -57,6 +60,13 @@ const Product = props => {
     await fetchAllProducts();
   }, [0]);
 
+  const checkAll = () => {
+    var inputs = document.querySelectorAll('.check');
+    for (var i = 0; i < inputs.length; i++) {
+      inputs[i].checked = true;
+    }
+  }
+
   const fetchProducts = async (availiblity, order_by, order, limit, offset) => {
     try {
       setIsLoader(true);
@@ -73,27 +83,25 @@ const Product = props => {
   }
 
   const sortProducts = async (event) => {
-    let setorder = '';
-    let orderby = '';
     switch (event.target.value) {
-      case 'DATE ASC':
-        setorder = 'ASC';
-        orderby = 'created_on';
+      case 'DATE DESC':
+        setOrder('DESC');
+        setOrderBy('created_on');
+        await fetchProducts('y', 'created_on', 'DESC', 10, 0);
         break;
 
       case 'PRICE ASC':
-        setorder = 'ASC';
-        orderby = 'price';
+        setOrder('ASC');
+        setOrderBy('price');
+        await fetchProducts('y', 'price', 'ASC', 10, 0);
         break;
 
       case 'PRICE DESC':
-        setorder = 'DESC';
-        orderby = 'price';
+        setOrder('DESC');
+        setOrderBy('price');
+        await fetchProducts('y', 'price', 'DESC', 10, 0);
         break;
     };
-    setOrder(setorder);
-    setOrderBy(orderby);
-    await fetchProducts('y', orderby, setorder, 10, 0);
   }
 
   const loadmore = async () => {
@@ -121,6 +129,16 @@ const Product = props => {
   }
 
   const filterByTitle = (value) => {
+
+    if (offer.length == 0 || category.length == 0 || color.length == 0 || brand.length == 0) {
+      addToast("Please re-load and re-navigate the page", {
+        appearance: 'warning',
+        autoDismiss: true,
+        placement: "bottom-center"
+      });
+      return false;
+    }
+
     switch (value.toLowerCase()) {
       case 'color':
         setFilterData(color);
@@ -146,14 +164,7 @@ const Product = props => {
         setFilterData(price);
         break;
     }
-
-    if (filterData.length == 0) {
-      addToast("Please re-load", {
-        appearance: 'error',
-        autoDismiss: true,
-        placement: "bottom-center"
-      });
-    }
+    setShowFilter(true);
   }
 
   const fetchAllProducts = async () => {
@@ -166,16 +177,40 @@ const Product = props => {
       offer: []
     };
     for (let i in response) {
-      data['brand'].push(titleCase(response[i]['brand']));
+      data['brand'].push({key: titleCase(response[i]['brand']), "isCheck": false});
       data['category'].push(titleCase(response[i]['category']));
       data['color'].push(titleCase(response[i]['color']));
-      data['offer'].push(titleCase(response[i]['offer']));
+      if ((response[i]['offer'].toLowerCase() !== "na") && (response[i]['offer'].toLowerCase() !== "no offer")) {
+        data['offer'].push(titleCase(response[i]['offer']));
+      }
     }
 
     setBrand([...new Set(data.brand)]);
     setCategory([...new Set(data.category)]);
     setColor([...new Set(data.color)]);
     setOffer([...new Set(data.offer)]);
+  }
+
+  const selectFilterValues = async (e, data) => {
+    //console.log(size, color, offer, category, brand, price);
+    let columnname = columnName;
+
+    if (offer.includes(data)) columnname = 'offer';
+    if (brand.includes(data)) columnname = 'brand';
+    if (category.includes(data)) columnname = 'category';
+    if (color.includes(data)) columnname = 'color';
+    if (size.includes(data)) columnname = 'size';
+    if (price.includes(data)) columnname = 'price';
+
+    setColumnName(columnname);
+
+    if (e.target.checked) {
+      if (!Object.keys(selectedData).includes(columnname)) selectedData[columnname] = [];
+      selectedData[columnname].push(data);
+    } else if (!e.target.checked){
+      const index = selectedData[columnname].indexOf(data);
+      selectedData[columnname].splice(index, 1);
+    }
   }
 
   return (
@@ -193,34 +228,40 @@ const Product = props => {
                 <li key={index} onClick={() => filterByTitle(filter.value)}>{filter.title}</li>
               ))}
             </ul>
-
-            <div>
-              {filterData.map((data, index) => (
-                <Fragment key={index}>
-                  <Input
-                    input={{
-                      type: "checkbox",
-                      name: "check",
-                      onChange: (e) => deleteSelectedUsers(e, admin.id)
-                    }}
-                  />
-                  <label>{data}</label>
-                </Fragment>
-              ))}
-            </div>
           </div>
 
           <div className={classes.sorting}>
             <label>Sort By: </label>
             <select onChange={sortProducts} defaultValue="">
               <option disabled value="">Select Option</option>
-              <option value="DATE ASC">New to Old</option>
+              <option value="DATE DESC">New to Old</option>
               <option value="PRICE ASC">Price: Low to High</option>
               <option value="PRICE DESC">Price: High to Low</option>
             </select>
           </div>
 
         </div>
+
+        {showFilter &&
+          <div className={classes.filterBar}>
+            <div className={classes.filterContent}>{filterData.map((data, index) => (
+              <Fragment key={index}>
+                <div>
+                  <Input
+                    input={{
+                      type: "checkbox",
+                      name: "check",
+                      onChange: (e) => selectFilterValues(e, data)
+                    }}
+                  />
+                  <label>{data}</label>
+                </div>
+              </Fragment>
+            ))}</div>
+
+            <button>Apply</button>
+          </div>
+        }
 
         <div className={classes.productsContainer}>
           {products?.length == 0 ? <h1>No Products</h1> :
